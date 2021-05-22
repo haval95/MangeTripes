031ce9b69f4c3b2e2369e538740e22e6f9a48671
haval95 <40795444+halenkurdi@users.noreply.github.com>
2021-05-22 15:17:59 +0300

    Feat: update trip functions
    all the functions in the update trip is working!

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 import { Route } from "react-router";
 import { Layout } from "./components/Layout";
 import { Home } from "./components/Home";
-import { FetchData } from "./components/FetchData";
-import { Counter } from "./components/Counter";
 
 import "./custom.css";
 import Trips from "./components/Trip/Trips";
 import CreateTrip from "./components/Trip/CreateTrip";
+import UpdateTrip from "./components/Trip/UpdateTrip";
 
 export default class App extends Component {
   static displayName = App.name;
@@ -16,10 +15,10 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path="/" component={Home} />
-        <Route path="/counter" component={Counter} />
-        <Route path="/fetch-data" component={FetchData} />
+
         <Route path="/trips" component={Trips} />
         <Route path="/create" component={CreateTrip} />
+        <Route path="/update/:id" component={UpdateTrip} />
       </Layout>
     );
   }
--- ClientApp/src/components/Trip/UpdateTrip.jsx
+++ ClientApp/src/components/Trip/UpdateTrip.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../custom.css";
-import { useHistory } from "react-router";
+import { useHistory, useParams } from "react-router";
 import axios from "axios";
-export default function CreateTrip() {
+export default function UpdateTrip() {
+  let { id } = useParams();
+
   const history = useHistory();
   const [state, setState] = useState({
     name: "",
@@ -10,21 +12,35 @@ export default function CreateTrip() {
     dateStarted: "",
     dateCompleted: "",
   });
+  useEffect(() => {
+    axios.get(`api/Trips/SingleTrip/${id}`).then((result) => {
+      let data = result.data;
+      setState({
+        name: data.name,
+        description: data.description,
+        dateStarted: new Date(data.dateStarted).toISOString().slice(0, 10),
+        dateCompleted: data.dateCompleted
+          ? new Date(data.dateCompleted).toISOString().slice(0, 10)
+          : null,
+      });
+    });
+  }, []);
   const handleChange = (e) => {
     setState({
       ...state,
       [e.target.name]: e.target.value,
     });
   };
-
+  const cancelEdit = () => {
+    history.push("/trips");
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
 
     let dataObj = {
-      id: Math.floor(Math.random() * 1000),
       ...state,
     };
-    axios.post("api/Trips/AddTrip", dataObj).then((result) => {
+    axios.put(`api/Trips/UpdateTrip/${id}`, dataObj).then((result) => {
       history.push("/trips");
     });
     setState({
@@ -85,14 +101,29 @@ export default function CreateTrip() {
             </div>
           </div>
         </div>
-
-        <div className="form-group">
-          <input
-            type="submit"
-            className="form-control"
-            value="Add Trip"
-            className="btn btn-primary"
-          />
+        <div className="row">
+          <div className="col-md-6 col-sm-6 col-xs-12">
+            <div className="form-group">
+              <input
+                type="button"
+                value="Cancel"
+                onClick={cancelEdit}
+                className="form-control"
+                value="Cancel"
+                className="btn btn-light"
+              />
+            </div>
+          </div>
+          <div className="col-md-6 col-sm-6 col-xs-12">
+            <div className="form-group">
+              <input
+                type="submit"
+                value="Update"
+                className="form-control"
+                className="btn btn-primary"
+              />
+            </div>
+          </div>
         </div>
       </form>
     </div>
